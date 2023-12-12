742930761afa804f50e64a56e2d08e2dca7d9a18
LORSCHEIDER Julien <julien.lorscheider8@etu.univ-lorraine.fr>
2023-12-12 17:40:14 +0100

    Initial Commit

---
+++ Time Laps/Time_LapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_LapsApp.swift
+//  Time Laps
+//
+//  Created by Julien on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Time_LapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
